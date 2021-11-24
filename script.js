c0ca7957c84e3a2e2977078ac88eef5b391a7e7c
MDWeb <mdweb@pc-261.home>
2021-11-24 09:27:35 +0100

    valid dice game

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // fonction stop game
 //fonction nom joueur
 //fonction pt max
+//fonction current player = point rouge a coté du houeur +couleur plus foncé (style font-weight=700)
 var players = [
     {
        scoreTotal : 0,
@@ -18,11 +19,13 @@ var currentPlayer = 0;
 var scoreWin = 10;
 
 function newGame(){
-    alert('Prét à jouer ?');
+
     $('#dice').replaceWith('<img class="dice" id="dice" value=1 alt="dice 1" src="img/dice_1.svg"></img>');
     for(let i = 0; i < playersLength; i++){
         players[i].scoreCurrent = 0;
         players[i].scoreTotal = 0;
+        //gerer le changement de joueur ?
+        $('#player_'+[i]).css('font-weight', '+=300');
         $('#current_score_p_'+[i]).html(0);
         $('#global_score_p_'+[i]).html(0);
     }
@@ -30,27 +33,17 @@ function newGame(){
 
 function stopGame(){
     if(players[0].scoreTotal > players[1].scoreTotal){
-        alert('Joueur 1 a gagné');
+        $("#winnerModal").modal('show');
+        $(".winner").html('Joueur 1 a gagné !');
     } else if(players[0].scoreTotal== players[1].scoreTotal){ 
-        alert('joueur à égalité');
+        $("#winnerModal").modal('show');
+        $(".winner").html('Vous avez tous les deux gagné !');
         } else {
-        alert('Joueur 2 a gagné');
+            $("#winnerModal").modal('show');
+            $(".winner").html('Joueur 2 a gagné !');
         }
     newGame();
 }
-
-
-
-// function stopGame(){
-//     var allScoreTotal = [];
-//     for(let i = 0; i < playersLength; i++){
-//         var scoreT = players[i].scoreTotal;
-//         alert(i);
-//         var total = allScoreTotal.push(scoreT);       
-//     }
-//     console.log(allScoreTotal);
-//     console.log(scoreT);
-// }
 
 function updateCurrent(score){
     if(score >= 1){
@@ -81,8 +74,11 @@ function changePlayer() {
 
 function getWinner(){
     if(players[currentPlayer].scoreTotal >= scoreWin){
+        $("#winnerModal").modal('show');
+       
         currentPlayer += 1;
-        alert('Joueur ' + currentPlayer + ' a gagné');
+        $(".winner").html('Joueur ' + currentPlayer + ' a gagné !');
+        // alert('Joueur ' + currentPlayer + ' a gagné');
         newGame();
         } else {
         players[currentPlayer].scoreCurrent = 0;
@@ -99,18 +95,21 @@ function hold(){
 
 $(document).ready(function(){ 
 
+    $('#rulesModal').modal('show');
+
     $('#new_game').click(function(){
-        alert('hello');
         newGame();
     })
-
+    $('#go_button').click(function(){
+        newGame();
+    })
     $('#stop_game').click(function(){
         stopGame();
     })
     $('#roll_dice').click(function(){
 
         
-        console.log('player ' + currentPlayer + ' - total: ' +  players[currentPlayer].scoreTotal + ' current:' + players[currentPlayer].scoreCurrent);
+        // console.log('player ' + currentPlayer + ' - total: ' +  players[currentPlayer].scoreTotal + ' current:' + players[currentPlayer].scoreCurrent);
         // console.log('joueur n=°'+currentPlayer);
         //selectionne un nombre au hasard entre 1 et §, affiche le dés correspondant et ajoute lavaleur au current score
         var number = Math.floor(Math.random() * 6) + 1;
